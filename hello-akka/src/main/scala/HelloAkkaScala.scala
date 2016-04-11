efa9967c46d40749bce133a701a38e6c98cf6aab
luhtonen <luhtonen@gmail.com>
2016-04-11 12:40:20 +0200

    Learning Akka video - Hello Akka application

---
+++ hello-akka/src/main/scala/HelloAkkaScala.scala
@@ -0,0 +1,23 @@
+import akka.actor.{Props, ActorSystem, Actor}
+
+// Define Actor Messages
+case class WhoToGreet(who: String)
+
+// Define Greeter Actor
+class Greeter extends Actor {
+  def receive = {
+    case WhoToGreet(who) => println(s"Hello $who")
+  }
+}
+
+object HelloAkkaScala extends App {
+
+  // Create the 'hello akka' actor system
+  val system = ActorSystem("Hello-Akka")
+
+  // Create the 'greeter' actor
+  val greeter = system.actorOf(Props[Greeter], "greeter")
+
+  // Send WhoToGreet Message to actor
+  greeter ! WhoToGreet("Akka")
+}
